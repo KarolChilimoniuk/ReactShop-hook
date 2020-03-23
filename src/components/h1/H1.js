5de746586111f069d76bcb7ca5930b4e7e8d55ee
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-03-23 20:26:28 +0100

    feat: added bigHeader css styles

--- src/components/h1/H1.js
+++ src/components/h1/H1.js
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "./H1.module.css";
 
 const H1 = ({ name }) => {
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className={style.bigHeader_container}>
+      <h1 className={style.bigHeader}>{name}</h1>
     </div>
   );
 };
