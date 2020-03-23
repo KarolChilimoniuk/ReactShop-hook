312936ab5d306c85ae5ca22a8760abe043f054d6
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-03-23 20:43:57 +0100

    modified: product css style and body background-color

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -3,8 +3,8 @@ import style from "./Product.module.css";
 
 const Products = ({ src, price, title }) => {
   return (
-    <div className={style.Product__container}>
-      <img src={src} alt="" />
+    <div className={style.product__container}>
+      <img className={style.product__image} src={src} alt="" />
       <p>{price}</p>
       <h3>{title}</h3>
     </div>
