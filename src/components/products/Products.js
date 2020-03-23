1d4d63df698d3ae4f1c7b43ce04f936360371376
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-03-23 20:39:13 +0100

    feat: added product css styles

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./Product.module.css";
 
 const Products = ({ src, price, title }) => {
   return (
-    <div>
+    <div className={style.Product__container}>
       <img src={src} alt="" />
       <p>{price}</p>
       <h3>{title}</h3>
